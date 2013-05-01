043691832d0d64bb89c51804ab3b8d387d2ca2aa
Akash Chauhan <akash6190@gmail.com>
2013-05-01 22:32:57 +0530

    Adding Initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BooksHr::Application.config.secret_token = '3c5bb2436ca4c0083ca13cb1c266b5514eaacccf9e0460925d6485a677273c5787d205c75555718677364269cfcb8c1bec88ffce8fc9a64582e788a0f7806aa6'
